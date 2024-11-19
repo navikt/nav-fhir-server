ae16dac4913a4b02eabff6854dd03afe396af053
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-11-19 13:57:22 +0100

    add cors policy for local development

--- src/main/kotlin/no/nav/helse/navfhirserver/security/SecurityConfig.kt
+++ src/main/kotlin/no/nav/helse/navfhirserver/security/SecurityConfig.kt
@@ -10,12 +10,15 @@ import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity(debug = true)
 class SecurityConfig() {
 
-    @Value("spring.security.oauth2.authorizationserver.issuer")
+    @Value("\${smart.configuration.jwks_uri}")
     private lateinit var jwksUri: String
 
     @Bean
@@ -23,23 +26,67 @@ class SecurityConfig() {
         http {
             authorizeHttpRequests {
                 // Create
-                authorize(HttpMethod.POST, "/User/**", hasAnyAuthority("SCOPE_user/*.c", "SCOPE_user/*.*"))
-                authorize(HttpMethod.POST, "/Patient/**", hasAnyAuthority("SCOPE_patient/*.c", "SCOPE_patient/*.*"))
-                authorize(HttpMethod.POST, "/Encounter/**", hasAnyAuthority("SCOPE_encounter/*.c", "SCOPE_encounter/*.*"))
+                authorize(
+                    HttpMethod.POST,
+                    "/User/**",
+                    hasAnyAuthority("SCOPE_user/*.c", "SCOPE_user/*.*")
+                )
+                authorize(
+                    HttpMethod.POST,
+                    "/Patient/**",
+                    hasAnyAuthority("SCOPE_patient/*.c", "SCOPE_patient/*.*")
+                )
+                authorize(
+                    HttpMethod.POST,
+                    "/Encounter/**",
+                    hasAnyAuthority("SCOPE_encounter/*.c", "SCOPE_encounter/*.*")
+                )
 
                 // Read
-                authorize(HttpMethod.GET, "/User/**", hasAnyAuthority("SCOPE_user/*.r", "SCOPE_user/*.*"))
-                authorize(HttpMethod.GET, "/Patient/**", hasAnyAuthority("SCOPE_patient/*.r", "SCOPE_patient/*.*"))
-                authorize(HttpMethod.GET, "/Encounter/**", hasAnyAuthority("SCOPE_encounter/*.r", "SCOPE_encounter/*.*"))
+                authorize(
+                    HttpMethod.GET,
+                    "/User/**",
+                    hasAnyAuthority("SCOPE_user/*.r", "SCOPE_user/*.*")
+                )
+                authorize(
+                    HttpMethod.GET,
+                    "/Patient/**",
+                    hasAnyAuthority("SCOPE_patient/*.r", "SCOPE_patient/*.*")
+                )
+                authorize(
+                    HttpMethod.GET,
+                    "/Encounter/**",
+                    hasAnyAuthority("SCOPE_encounter/*.r", "SCOPE_encounter/*.*")
+                )
 
                 // Update
-                authorize(HttpMethod.PUT, "/User/**", hasAnyAuthority("SCOPE_user/*.u", "SCOPE_user/*.*"))
-                authorize(HttpMethod.PUT, "/Patient/**", hasAnyAuthority("SCOPE_patient/*.u", "SCOPE_patient/*.*"))
-                authorize(HttpMethod.PUT, "/Encounter/**", hasAnyAuthority("SCOPE_encounter/*.u", "SCOPE_encounter/*.*"))
+                authorize(
+                    HttpMethod.PUT,
+                    "/User/**",
+                    hasAnyAuthority("SCOPE_user/*.u", "SCOPE_user/*.*")
+                )
+                authorize(
+                    HttpMethod.PUT,
+                    "/Patient/**",
+                    hasAnyAuthority("SCOPE_patient/*.u", "SCOPE_patient/*.*")
+                )
+                authorize(
+                    HttpMethod.PUT,
+                    "/Encounter/**",
+                    hasAnyAuthority("SCOPE_encounter/*.u", "SCOPE_encounter/*.*")
+                )
 
                 // Delete
-                authorize(HttpMethod.DELETE, "/User/**", hasAnyAuthority("SCOPE_user/*.d", "SCOPE_user/*.*"))
-                authorize(HttpMethod.DELETE, "/Patient/**", hasAnyAuthority("SCOPE_patient/*.d", "SCOPE_patient/*.*"))
+                authorize(
+                    HttpMethod.DELETE,
+                    "/User/**",
+                    hasAnyAuthority("SCOPE_user/*.d", "SCOPE_user/*.*")
+                )
+                authorize(
+                    HttpMethod.DELETE,
+                    "/Patient/**",
+                    hasAnyAuthority("SCOPE_patient/*.d", "SCOPE_patient/*.*")
+                )
                 authorize(
                     HttpMethod.DELETE,
                     "/Encounter/**",
@@ -47,8 +94,9 @@ class SecurityConfig() {
                 )
 
                 // General
-                authorize("/.well-known/smart-configuration", permitAll)
+                authorize("/.well-known/**", permitAll)
                 authorize("/metadata", permitAll)
+                authorize("/epj", permitAll)
                 authorize(anyRequest, authenticated)
             }
             oauth2ResourceServer {
@@ -56,9 +104,26 @@ class SecurityConfig() {
                     jwtDecoder = jwtDecoder()
                 }
             }
+            cors {
+                configurationSource = corsConfigurationSource()
+            }
             csrf { disable() }
         }
         return http.build()
+    }
+
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration().apply {
+            allowedOrigins = listOf("http://localhost:5173")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
+            allowCredentials = true
+            maxAge = 3600L
+        }
+
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
     }
 
     fun jwtDecoder(): JwtDecoder {
