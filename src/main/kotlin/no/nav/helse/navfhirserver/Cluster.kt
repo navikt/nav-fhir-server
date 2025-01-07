a4e86e850cfe3dfd71b0ddbf1ad3e97154d43907
Jan-Olav Eide <jan-olav,eide@nav.no>
2025-01-07 09:29:19 +0100

    auth.fhir.ekstern.dev.nav.no bruker en protokoll som ikke støttes.

--- src/main/kotlin/no/nav/helse/navfhirserver/Cluster.kt
+++ src/main/kotlin/no/nav/helse/navfhirserver/Cluster.kt
@@ -5,8 +5,6 @@ import no.nav.helse.navfhirserver.Constants.DEV_GCP
 import no.nav.helse.navfhirserver.Constants.GCP
 import no.nav.helse.navfhirserver.Constants.LOCAL
 import no.nav.helse.navfhirserver.Constants.NAIS_CLUSTER_NAME
-import no.nav.helse.navfhirserver.Constants.NAIS_IMAGE_NAME
-import no.nav.helse.navfhirserver.Constants.NAIS_NAMESPACE_NAME
 import no.nav.helse.navfhirserver.Constants.PROD
 import no.nav.helse.navfhirserver.Constants.PROD_GCP
 import no.nav.helse.navfhirserver.Constants.TEST
@@ -22,8 +20,6 @@ internal object Constants {
     internal  const val DEV_GCP = "${DEV}-${GCP}"
     internal const val PROD_GCP = "${PROD}-${GCP}"
     internal const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"
-    internal const val NAIS_NAMESPACE_NAME = "NAIS_NAMESPACE"
-    internal  const val NAIS_IMAGE_NAME = "NAIS_APP_IMAGE"
 }
 
 internal enum class Cluster(private val clusterName: String) {
@@ -35,20 +31,21 @@ internal enum class Cluster(private val clusterName: String) {
     companion object {
 
         private val cluster = getenv(NAIS_CLUSTER_NAME) ?: LOCAL
-        val current = Cluster.entries.firstOrNull { it.clusterName == cluster } ?: LOCAL_CLUSTER
-        val image = getenv(NAIS_IMAGE_NAME) ?: "n/a"
-        val namespace = getenv(NAIS_NAMESPACE_NAME) ?: LOCAL
+        private val PROD_PROFILES = arrayOf(PROD, PROD_GCP, GCP)
+        private val DEV_PROFILES = arrayOf(DEV, DEV_GCP, GCP)
+
+        val current = entries.firstOrNull { it.clusterName == cluster } ?: LOCAL_CLUSTER
         val isProd = current == PROD_GCP_CLUSTER
         val isDev = current == DEV_GCP_CLUSTER
-        val isNais = current in listOf(DEV_GCP_CLUSTER, PROD_GCP_CLUSTER)
+        val isNais = isDev || isProd
         val profiler =
             when (current) {
                 TEST_CLUSTER, LOCAL_CLUSTER ->
                     arrayOf(current.clusterName).also {
-                        setProperty(NAIS_CLUSTER_NAME, current.clusterName)
+                        setProperty(NAIS_CLUSTER_NAME, it.single())
                     }
-                DEV_GCP_CLUSTER -> arrayOf(DEV, DEV_GCP, GCP)
-                PROD_GCP_CLUSTER -> arrayOf(PROD, PROD_GCP, GCP)
+                DEV_GCP_CLUSTER -> DEV_PROFILES
+                PROD_GCP_CLUSTER -> PROD_PROFILES
             }
     }
 }
