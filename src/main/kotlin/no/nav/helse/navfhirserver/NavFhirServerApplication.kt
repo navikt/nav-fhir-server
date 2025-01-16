80a70138e97eb65329a349b68c3900a20d863ce7
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2025-01-16 14:51:46 +0100

    change port

--- src/main/kotlin/no/nav/helse/navfhirserver/NavFhirServerApplication.kt
+++ src/main/kotlin/no/nav/helse/navfhirserver/NavFhirServerApplication.kt
@@ -4,18 +4,16 @@ import no.nav.helse.navfhirserver.Cluster.Companion.profiler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableWebMvc
 @ConfigurationPropertiesScan("no.nav.helse.navfhirserver")
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
-open class NavFhirServerApplication
+class NavFhirServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<NavFhirServerApplication>(*args)  {
-        setAdditionalProfiles(*profiler)
-    }
+  runApplication<NavFhirServerApplication>(*args) {
+    setAdditionalProfiles(*profiler)
+  }
 }
 
--- src/main/kotlin/no/nav/helse/navfhirserver/security/SecurityConfig.kt
+++ src/main/kotlin/no/nav/helse/navfhirserver/security/SecurityConfig.kt
@@ -12,106 +12,107 @@ import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
-class SecurityConfig() {
+@EnableWebSecurity
+class SecurityConfig {
 
-    @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http {
-            authorizeHttpRequests {
-                // Create
-                authorize(
-                    HttpMethod.POST,
-                    "/User/**",
-                    hasAnyAuthority("SCOPE_user/*.c", "SCOPE_user/*.*"),
-                )
-                authorize(
-                    HttpMethod.POST,
-                    "/Patient/**",
-                    hasAnyAuthority("SCOPE_patient/*.c", "SCOPE_patient/*.*"),
-                )
-                authorize(
-                    HttpMethod.POST,
-                    "/Encounter/**",
-                    hasAnyAuthority("SCOPE_encounter/*.c", "SCOPE_encounter/*.*"),
-                )
+  @Bean
+  fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    http {
+      authorizeHttpRequests {
+        // Create
+        authorize(
+          HttpMethod.POST,
+          "/User/**",
+          hasAnyAuthority("SCOPE_user/*.c", "SCOPE_user/*.*"),
+        )
+        authorize(
+          HttpMethod.POST,
+          "/Patient/**",
+          hasAnyAuthority("SCOPE_patient/*.c", "SCOPE_patient/*.*"),
+        )
+        authorize(
+          HttpMethod.POST,
+          "/Encounter/**",
+          hasAnyAuthority("SCOPE_encounter/*.c", "SCOPE_encounter/*.*"),
+        )
 
-                // Read
-                authorize(
-                    HttpMethod.GET,
-                    "/User/**",
-                    hasAnyAuthority("SCOPE_user/*.r", "SCOPE_user/*.*"),
-                )
-                authorize(
-                    HttpMethod.GET,
-                    "/Patient/**",
-                    hasAnyAuthority("SCOPE_patient/*.r", "SCOPE_patient/*.*"),
-                )
-                authorize(
-                    HttpMethod.GET,
-                    "/Encounter/**",
-                    hasAnyAuthority("SCOPE_encounter/*.r", "SCOPE_encounter/*.*"),
-                )
+        // Read
+        authorize(
+          HttpMethod.GET,
+          "/User/**",
+          hasAnyAuthority("SCOPE_user/*.r", "SCOPE_user/*.*"),
+        )
+        authorize(
+          HttpMethod.GET,
+          "/Patient/**",
+          hasAnyAuthority("SCOPE_patient/*.r", "SCOPE_patient/*.*"),
+        )
+        authorize(
+          HttpMethod.GET,
+          "/Encounter/**",
+          hasAnyAuthority("SCOPE_encounter/*.r", "SCOPE_encounter/*.*"),
+        )
 
-                // Update
-                authorize(
-                    HttpMethod.PUT,
-                    "/User/**",
-                    hasAnyAuthority("SCOPE_user/*.u", "SCOPE_user/*.*"),
-                )
-                authorize(
-                    HttpMethod.PUT,
-                    "/Patient/**",
-                    hasAnyAuthority("SCOPE_patient/*.u", "SCOPE_patient/*.*"),
-                )
-                authorize(
-                    HttpMethod.PUT,
-                    "/Encounter/**",
-                    hasAnyAuthority("SCOPE_encounter/*.u", "SCOPE_encounter/*.*"),
-                )
+        // Update
+        authorize(
+          HttpMethod.PUT,
+          "/User/**",
+          hasAnyAuthority("SCOPE_user/*.u", "SCOPE_user/*.*"),
+        )
+        authorize(
+          HttpMethod.PUT,
+          "/Patient/**",
+          hasAnyAuthority("SCOPE_patient/*.u", "SCOPE_patient/*.*"),
+        )
+        authorize(
+          HttpMethod.PUT,
+          "/Encounter/**",
+          hasAnyAuthority("SCOPE_encounter/*.u", "SCOPE_encounter/*.*"),
+        )
 
-                // Delete
-                authorize(
-                    HttpMethod.DELETE,
-                    "/User/**",
-                    hasAnyAuthority("SCOPE_user/*.d", "SCOPE_user/*.*"),
-                )
-                authorize(
-                    HttpMethod.DELETE,
-                    "/Patient/**",
-                    hasAnyAuthority("SCOPE_patient/*.d", "SCOPE_patient/*.*"),
-                )
-                authorize(
-                    HttpMethod.DELETE,
-                    "/Encounter/**",
-                    hasAnyAuthority("SCOPE_encounter/*.d", "SCOPE_encounter/*.*"),
-                )
+        // Delete
+        authorize(
+          HttpMethod.DELETE,
+          "/User/**",
+          hasAnyAuthority("SCOPE_user/*.d", "SCOPE_user/*.*"),
+        )
+        authorize(
+          HttpMethod.DELETE,
+          "/Patient/**",
+          hasAnyAuthority("SCOPE_patient/*.d", "SCOPE_patient/*.*"),
+        )
+        authorize(
+          HttpMethod.DELETE,
+          "/Encounter/**",
+          hasAnyAuthority("SCOPE_encounter/*.d", "SCOPE_encounter/*.*"),
+        )
 
-                // Not accessible on public ingress anyway
-                authorize("/internal/**", permitAll)
+        // Not accessible on public ingress anyway
+        authorize("/internal/**", permitAll)
 
-                // General
-                authorize("/.well-known/**", permitAll)
-                authorize("/metadata", permitAll)
-                authorize("/error/**", permitAll)
-                authorize(anyRequest, authenticated)
-            }
-            cors { configurationSource = corsConfigurationSource() }
-            csrf { disable() }
-        }
-        return http.build()
+        // General
+        authorize("/.well-known/**", permitAll)
+        authorize("/metadata", permitAll)
+        authorize("/error/**", permitAll)
+        authorize(anyRequest, authenticated)
+      }
+      cors { configurationSource = corsConfigurationSource() }
+      csrf { disable() }
     }
+    return http.build()
+  }
 
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val config =
-            CorsConfiguration().apply {
-                allowedOriginPatterns =
-                    listOf("https://*.dev.nav.no", "http://localhost:[*]", "http://127.0.0.1:[*]")
-                allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
-                allowCredentials = true
-                maxAge = 3600L
-            }
+  fun corsConfigurationSource(): CorsConfigurationSource {
+    val config =
+      CorsConfiguration().apply {
+        allowedOriginPatterns =
+          listOf("https://*.dev.nav.no", "http://localhost:[*]", "http://127.0.0.1:[*]")
+        allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
+        allowCredentials = true
+        maxAge = 3600L
+      }
 
-        return UrlBasedCorsConfigurationSource().apply { registerCorsConfiguration("/**", config) }
-    }
+    return UrlBasedCorsConfigurationSource().apply { registerCorsConfiguration("/**", config) }
+  }
 }
